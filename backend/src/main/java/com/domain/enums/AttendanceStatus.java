e9e394ba27c4469d5da5484b8bd6801bc5e43b86
Paus <paus@example.com>
2026-01-13 15:52:50 +0700

    up

--- backend/src/main/java/com/domain/enums/AttendanceStatus.java
+++
@@ -1,7 +0,0 @@
-package com.domain.enums;
-
-public enum AttendanceStatus {
-  ON_TIME,
-  LATE,
-  ABSENT
-}
--- backend/src/main/java/com/domain/enums/Role.java
+++
@@ -1,6 +0,0 @@
-package com.domain.enums;
-
-public enum Role {
-  TEACHER,
-  STUDENT
-}
