597da5b5cd6405e3421577b3c8ce788e903c2253
Paus <paus@example.com>
2026-01-17 21:10:49 +0700

    up

--- backend/src/main/java/com/api/dto/StudentDtos.java
+++
@@ -1,7 +0,0 @@
-package com.api.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public class StudentDtos {
-  public record JoinClassRequest(@NotBlank String classCode) {}
-}
--- backend/src/main/java/com/domain/enums/SessionStatus.java
+++
@@ -1,6 +0,0 @@
-package com.domain.enums;
-
-public enum SessionStatus {
-  OPEN,
-  CLOSED
-}
